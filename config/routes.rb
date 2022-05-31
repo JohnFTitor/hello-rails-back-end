a388c719aed97c5ac2ebcdf8a37be92283d2fdd9
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-31 10:33:44 -0500

    Generate a random hello message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  namespace :v1, defaults: { format: 'json' } do
+    resources :greetings, only: %i[index]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
